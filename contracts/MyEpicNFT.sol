fa8ac54ef75ad71b51e334d114b70ed678773c85
superstar <83311833+clever-web@users.noreply.github.com>
2022-05-06 03:34:37 +0300

    env-resetup

---
+++ contracts/MyEpicNFT.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.1;
+
+import "hardhat/console.sol";
+
+contract MyEpicNFT {
+    constructor() {
+        console.log("This is my NFT contract. Whoa!");
+    }
+}
